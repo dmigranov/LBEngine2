a32fe62898d61a6f3a38860fe144f663a562b01a
Denis Migranov <d.migranov@g.nsu.ru>
2022-07-18 15:41:08 +0700

    Added example main from LBEngine1

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -1,74 +1,293 @@
-﻿#pragma once
+﻿#include <pch.h>
 
-#include "Geometries/ToricGeometry.h"
+#include "Game.h"
+#include "Scene.h"
+#include "ResourceManager.h"
+#include "InputComponent.h"
+#include "Entity.h"
+
+// Components
+#include "ComponentTypeManager.h"
+
+#include "PlayerComponent.h"
+#include "TransformComponent.h"
+#include "InputComponent.h"
+#include "BitmapComponent.h"
+#include "CameraComponent.h"
+#include "WalkComponent.h"
+#include "PhysicsComponent.h"
+#include "CollisionComponent.h"
+#include "AABBCollisionComponent.h"
+
+#include "Force.h"
+#include "Impulse.h"
+
+
+// Systems
+#include "TransformUpdateSystem.h"
+#include "InputSystem.h"
+#include "BitmapRenderSystem.h"
+#include "ActionSystem.h"
+#include "CameraActionSystem.h"
+#include "PhysicsSystem.h"
+#include "CollisionSystem.h"
 
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
+
+template <typename T> int sgn(T val)
+{
+    return (T(0) < val) - (val < T(0));
+}
+
+// Entry point
 int main(int argc, char* argv[])
 {
-    auto scene = InitializeToricGeometry(L"Test game", DirectX::Colors::PowderBlue,
-        10, DirectX::SimpleMath::Vector3(30, 30, 30), 20, 0.9,
-        true, false, false);
+    //todo: Component* -> Component&  ?
 
-    auto resourceManager = Game::GetInstance().GetResourceManager();
-    Texture* cubemapTexture = new Texture(L"cubemap.dds");
+    Game& game = Game::GetInstance();
+    game.InitializeEngine(L"Test game", true, false, false);
+    game.SetBackgroundColor(DirectX::Colors::PowderBlue);   //todo: перенести
+    Scene* scene = game.GetScene();
+    auto resourceManager = game.GetResourceManager();
 
-
-    Entity* cameraEntity = new Entity("camera1");
-    auto cameraTransform = new ToricTransformComponent();
-    auto cameraComponent = new CameraComponent();
-    cameraComponent->SetFovY(DirectX::XM_PI / 3);
-    cameraEntity->AddComponent<ToricTransformComponent>(cameraTransform);
-    cameraEntity->AddComponent<CameraComponent>(cameraComponent);
-    cameraEntity->AddComponent<InputComponent>(new InputComponent());
-    scene->SetCamera(cameraEntity, cameraComponent);
-    scene->AddEntity(cameraEntity);
-
-    auto effect = new ToricExpFogEffect(cubemapTexture, 0.002, DirectX::Colors::PowderBlue);
-
-    auto charInputComponent = new InputComponent();
-
-    auto entity1 = new Entity();
-    auto ttc1 = new ToricTransformComponent();
     {
-        auto tmc1 = EuclideanMeshComponentFactory::CreateCube(3);
-        tmc1->SetEffect(effect);
-        entity1->AddComponent<ToricTransformComponent>(ttc1);
-        entity1->AddComponent<MeshComponent>(tmc1);
-        entity1->AddComponent<InputComponent>(charInputComponent);
-        scene->AddEntity(entity1);
+        auto componentTypeManager = game.GetComponentTypeManager();
+        componentTypeManager->RegisterComponentType<PlayerComponent>();
+        componentTypeManager->RegisterComponentType<CameraComponent>();
+        componentTypeManager->RegisterComponentType<InputComponent>();
+        componentTypeManager->RegisterComponentType<BitmapComponent>();
+        componentTypeManager->RegisterComponentType<WalkComponent>();
+        componentTypeManager->RegisterComponentType<PhysicsComponent>();
+        componentTypeManager->RegisterComponentType<CollisionComponent>();
 
-        entity1->AddComponent<UpdaterComponent>(new UpdaterComponent([](double deltaTime, Entity* pEntity) {
-            auto pTransform = pEntity->GetComponent<ToricTransformComponent>();
-            auto pInput = pEntity->GetComponent<InputComponent>();
-            auto kbs = pInput->GetKeyboardState();
-            auto ms = pInput->GetMouseState();
-
-            Vector3 up(0, deltaTime * 200, 0);
-            Vector3 right(deltaTime * 200, 0, 0);
-            Vector3 fwd(0, 0, deltaTime * 200);
-
-            if (kbs.R)
-                pTransform->Move(up);
-            if (kbs.F)
-                pTransform->Move(-up);
-
-            if (kbs.T)
-                pTransform->Rotate(deltaTime, 0, 0);
-            if (kbs.Y)
-                pTransform->Rotate(-deltaTime, 0, 0);
-            if (kbs.G)
-                pTransform->Rotate(0, deltaTime, 0);
-            if (kbs.H)
-                pTransform->Rotate(0, -deltaTime, 0);
-            if (kbs.B)
-                pTransform->Rotate(0, 0, deltaTime);
-            if (kbs.N)
-                pTransform->Rotate(0, 0, -deltaTime);
-
-            }));
+        componentTypeManager->SetTypeAdditionEnded();
     }
 
-    return Game::GetInstance().StartGame();
+    Texture* brickTexture = resourceManager->CreateTexture(L"brick.dds");
+    Texture* characterTexture = resourceManager->CreateTexture(L"char2.dds");
+
+    scene->AddSystem(new InputSystem());
+    scene->AddSystem(new BitmapRenderSystem());
+    scene->AddSystem(new PhysicsSystem());
+    scene->AddSystem(new CollisionSystem());
+
+    auto cameraTransform = new TransformComponent(0, 0, -1, 0, 0, 0);
+
+
+    scene->AddSystem(new ActionSystem<InputComponent, TransformComponent, WalkComponent, PhysicsComponent>([](Entity* pEntity, double deltaTime) {
+        auto pTransform = pEntity->GetTransform();
+        auto pInput = pEntity->GetComponent<InputComponent>();
+        auto kbs = pInput->GetKeyboardState();
+        auto ms = pInput->GetMouseState();
+        auto pWalk = pEntity->GetComponent<WalkComponent>();
+        auto pPhysics = pEntity->GetComponent<PhysicsComponent>();
+
+        Vector3 up(0, deltaTime * pWalk->m_movementGain, 0);
+        Vector3 right(deltaTime * pWalk->m_movementGain, 0, 0);
+
+
+        //с помощью импульсов и сил можно реализовывать движение на постоянной скорости, 
+        //но это не входит в мои цели
+        static bool isRightPressed;
+        if (kbs.D)
+        {
+            if (!isRightPressed)
+            {
+                pPhysics->AddForce("right", Force(Vector3(10., 0, 0.)));
+                isRightPressed = true;
+            }
+
+        }
+        else if (isRightPressed)
+        {
+            pPhysics->RemoveForce("right");
+            isRightPressed = false;
+        }
+
+        static bool isLeftPressed;
+        if (kbs.A)
+        {
+            if (!isLeftPressed)
+            {
+                pPhysics->AddForce("left", Force(Vector3(-10., 0, 0.)));
+
+                isLeftPressed = true;
+            }
+        }
+        else if (isLeftPressed)
+        {
+            pPhysics->RemoveForce("left");
+            isLeftPressed = false;
+        }
+
+        static bool isSpacePressed;
+        if (kbs.Space)
+        {
+            if (!isSpacePressed)
+            {
+                pPhysics->AddForce("jump", Force(Vector3(0., 15, 0.)));
+                isSpacePressed = true;
+            }
+        }
+        else if (isSpacePressed)
+        {
+            pPhysics->RemoveForce("jump");
+            isSpacePressed = false;
+        }
+        }));
+
+    Entity* cameraEntity = new Entity("camera1");
+    auto cameraComponent = new CameraComponent(false);
+    cameraComponent->SetOrthogonalWidth(30.);
+
+    cameraEntity->SetTransform(cameraTransform);
+    cameraEntity->AddComponent<CameraComponent>(cameraComponent);
+    cameraEntity->AddComponent<InputComponent>(new InputComponent());
+
+    scene->AddEntity(cameraEntity);
+    scene->SetCamera(cameraEntity);
+
+    auto brickCollisionComponent = new AABBCollisionComponent([](Entity* pThisEntity, Entity* pOtherEntity, double deltaTime) {}, -0.5, -0.5, 0.5, 0.5);
+    auto brickPhysicsComponent = new PhysicsComponent(10.);
+
+    auto brickBitmap = new BitmapComponent(1, 1, brickTexture, false);
+
+    constexpr int wh = 25;
+    constexpr int brickCount = wh * wh - (wh - 2) * (wh - 3);
+
+    Entity* bricks[brickCount];
+    TransformComponent* transforms[brickCount];
+
+
+    int j = 0;
+    for (int i = 0; i < wh * wh; i++)
+    {
+        int ColumnNumber = i % wh;
+        int RowNumber = i / wh;
+        if (ColumnNumber == 0 || RowNumber == 0 || ColumnNumber == wh - 1)
+        {
+            auto e = bricks[j] = new Entity();
+            auto t = transforms[j] = new TransformComponent(ColumnNumber, RowNumber, 1, 0, 0, 0, 1, 1, 1);
+            j++;
+            e->SetTransform(t);
+            e->AddComponent<BitmapComponent>(brickBitmap);
+            e->AddComponent<PhysicsComponent>(brickPhysicsComponent);
+            e->AddComponent<CollisionComponent>(brickCollisionComponent);
+            scene->AddEntity(e);
+        }
+    }
+
+
+    auto charBitmap = new BitmapComponent(1, 1, characterTexture, false);
+    auto charTransform = new TransformComponent(1, 3, 0, 0, 0, 0, 1, 1, 1);
+    auto charWalkComponent = new WalkComponent(3, 4);
+    auto charPhysicsComponent = new PhysicsComponent(1.);
+    auto charInputComponent = new InputComponent();
+    auto charCollisionComponent = new AABBCollisionComponent([](Entity* pThisEntity, Entity* pOtherEntity, double deltaTime) {
+
+        auto pPhysics = pThisEntity->GetComponent<PhysicsComponent>();
+        auto pTransform = pThisEntity->GetComponent<TransformComponent>();
+
+        auto velocity = pPhysics->GetVelocity();
+        auto acceleration = pPhysics->GetAcceleration();
+
+        pTransform->Move(-velocity * deltaTime);
+
+        //todo: поэлегантнее!
+
+        pPhysics->SetAcceleration(Vector3::Zero);
+        pPhysics->SetVelocity(Vector3::Zero);
+        }, -0.5, -0.5, 0.5, 0.5, true);
+
+
+
+    charPhysicsComponent->AddForce("gravity", Force(Vector3(0, -9.8, 0)));
+    charPhysicsComponent->AddForce("drag", Force([charPhysicsComponent](Force& force)
+        {
+            static double vxPrevAbs = 0.;
+
+            auto v = charPhysicsComponent->GetVelocity();
+
+            if (abs(v.x) < 0.1 && vxPrevAbs > abs(v.x))
+                charPhysicsComponent->SetVelocity(Vector3(0, v.y, v.z));
+
+            //std::cout << v.x << " " << v.y << std::endl;
+
+            //auto newV = Vector3(-sgn<float>(v.x)* v.x * v.x / 2, -sgn<float>(v.y) * v.y * v.y / 2, v.z);
+            //force.SetVector(newV);
+
+            //todo: а должна ли drag force действовать при движении вниз?
+            force.SetVector(-v / 2);
+
+            vxPrevAbs = abs(v.x);
+        }));
+
+    Entity* character = new Entity();
+
+    character->SetTransform(charTransform);
+    character->AddComponent<BitmapComponent>(charBitmap);
+    character->AddComponent<WalkComponent>(charWalkComponent);
+    character->AddComponent<PhysicsComponent>(charPhysicsComponent);
+    character->AddComponent<InputComponent>(charInputComponent);
+    character->AddComponent<CollisionComponent>(charCollisionComponent);
+
+
+    cameraTransform->SetParent(charTransform);
+
+    scene->AddEntity(character);
+
+    return game.StartGame();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    MeshComponent::VertexPosTex vertices[8] = {
+        { XMFLOAT4(-1.0f, -1.0f, -1.0f, 1.0f), XMFLOAT2(0, 0) }, // 0
+        { XMFLOAT4(-1.0f,  1.0f, -1.0f, 1.0f), XMFLOAT2(0, 1) }, // 1
+        { XMFLOAT4(1.0f,  1.0f, -1.0f, 1.0f), XMFLOAT2(1, 1)}, // 2
+        { XMFLOAT4(1.0f, -1.0f, -1.0f, 1.0f), XMFLOAT2(1, 0)}, // 3
+        { XMFLOAT4(-1.0f, -1.0f,  1.0f, 1.0f),  XMFLOAT2(0, 0) }, // 4
+        { XMFLOAT4(-1.0f,  1.0f,  1.0f, 1.0f), XMFLOAT2(0, 1)}, // 5
+        { XMFLOAT4(1.0f,  1.0f,  1.0f, 1.0f), XMFLOAT2(1, 1) }, // 6
+        { XMFLOAT4(1.0f, -1.0f,  1.0f, 1.0f), XMFLOAT2(0, 1) }  // 7
+    };
+
+    WORD indices[36] =
+    {
+        0, 1, 2, 0, 2, 3,
+        4, 6, 5, 4, 7, 6,
+        4, 5, 1, 4, 1, 0,
+        3, 2, 6, 3, 6, 7,
+        1, 5, 6, 1, 6, 2,
+        3, 4, 0, 7, 4, 3
+    };
+    auto m = new MeshComponent(8, vertices, 36, indices);
+*/

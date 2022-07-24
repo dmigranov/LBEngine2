3a75a15f9c442e1902b1e709869ac90456a1de53
Denis Migranov <d.migranov@g.nsu.ru>
2022-07-24 19:01:51 +0700

    some additions to BasicBitmapEffect header

--- LBEngine/inc/BasicBitmapEffect.h
+++ LBEngine/inc/BasicBitmapEffect.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "Effect.h"
+
+class Game;
+class Texture;
+
 class BasicBitmapEffect :
     public Effect
 {
@@ -25,6 +29,51 @@ class BasicBitmapEffect :
         DirectX::XMVECTORF32 fogColor = DirectX::Colors::CadetBlue;
     };
 
+    BasicBitmapEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor);
 
+    virtual bool Initialize() override;
+    virtual void Deinitialize() override;
+
+    virtual void UpdatePerObject(const Entity* pEntity, double deltaTime) override;
+    virtual void UpdatePerScene() override;             //todo: impl
+    virtual void UpdatePerApplication() override;       //todo: impl
+
+    virtual void Clean() override;
+
+    virtual unsigned int GetVertexBufferSize() const override;
+    virtual const std::type_info& GetVertexDataType() override;
+
+    void SetFogColor(DirectX::XMVECTORF32 fogColor);
+
+    ~BasicBitmapEffect();
+
+private:
+    PerApplicationPSConstantBuffer perApplicationPSConstantBuffer;
+    PerApplicationVSConstantBuffer perApplicationVSConstantBuffer;
+
+    ID3D11InputLayout* g_d3dInputLayout = nullptr;
+
+    ID3D11VertexShader* g_d3dVertexShader = nullptr;
+
+    ID3D11PixelShader* g_d3dPixelShader = nullptr;
+
+    ID3D11SamplerState* g_d3dSamplerState = nullptr;
+
+    Texture* m_pTexture = nullptr;
+
+    enum ConstantBuffer
+    {
+        CB_Application, //The application level constant buffer stores variables that rarely change. 
+        CB_Frame,       //The frame level constant buffer stores variables that change each frame. An example of a frame level shader variable would be the camera’s view matrix which changes whenever the camera moves
+        CB_Object,      //The object level constant buffer stores variables that are different for every object being rendered. An example of an object level shader variable is the object’s world matrix.
+        NumConstantBuffers
+    };
+    //three constant buffers: buffers are used to store shader variables that remain constant during current draw call. An example of a constant shader variable is the camera’s projection matrix. Since the projection matrix will be the same for every vertex of the object, this variable does not need to be passed to the shader using vertex data.
+
+    ID3D11Buffer* g_d3dVSConstantBuffers[NumConstantBuffers];
+
+    ID3D11Buffer* g_d3dPSConstantBuffer = nullptr;
+
+    Game& game;
 };
 

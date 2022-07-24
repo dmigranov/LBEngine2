7523ae310a9fc180827e05542df7160d21c41104
Denis Migranov <d.migranov@g.nsu.ru>
2022-07-24 18:56:30 +0700

    added structs for the effect

--- LBEngine/inc/BasicBitmapEffect.h
+++ LBEngine/inc/BasicBitmapEffect.h
@@ -3,5 +3,28 @@
 class BasicBitmapEffect :
     public Effect
 {
+    struct PerApplicationVSConstantBuffer
+    {
+        DirectX::XMMATRIX proj;
+        double density = 0.1;
+        //double pad;
+    };
+
+    struct PerFrameVSConstantBuffer
+    {
+        DirectX::XMMATRIX view;
+    };
+
+    struct PerObjectVSConstantBuffer
+    {
+        DirectX::XMMATRIX world;
+    };
+
+    struct PerApplicationPSConstantBuffer
+    {
+        DirectX::XMVECTORF32 fogColor = DirectX::Colors::CadetBlue;
+    };
+
+
 };
 

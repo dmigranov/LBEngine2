f205d6a78cdca1de82b1c0d04f7bab4db68b5aeb
Denis Migranov <d.migranov@g.nsu.ru>
2022-07-24 19:41:56 +0700

    It doesn't render still

---
+++ LBEngine/Data/Shaders/BasicFogPixelShader.hlsl
@@ -0,0 +1,21 @@
+Texture2D shaderTexture;
+SamplerState SampleType;
+
+cbuffer PerApplication : register(b0)
+{
+	float4 fogColor;
+}
+
+struct PixelShaderInput
+{
+	float2 tex : TEXCOORD0;
+	float fogFactor : FOG_FACTOR;
+};
+ 
+float4 SimplePixelShader(PixelShaderInput IN) : SV_TARGET
+{
+	float4 sourceColor = ((IN.tex.x == 0.f && IN.tex.y == 0.f) ? 1.f : shaderTexture.Sample(SampleType, IN.tex));
+	float4 retColor = IN.fogFactor * sourceColor + (1.0 - IN.fogFactor) * fogColor;
+
+	return retColor;
+}
--- LBEngine/Data/Shaders/BasicPixelShader.hlsl
+++ LBEngine/Data/Shaders/BasicPixelShader.hlsl
@@ -9,13 +9,11 @@ cbuffer PerApplication : register(b0)
 struct PixelShaderInput
 {
 	float2 tex : TEXCOORD0;
-	float fogFactor : FOG_FACTOR;
 };
  
 float4 SimplePixelShader(PixelShaderInput IN) : SV_TARGET
 {
 	float4 sourceColor = ((IN.tex.x == 0.f && IN.tex.y == 0.f) ? 1.f : shaderTexture.Sample(SampleType, IN.tex));
-	float4 retColor = IN.fogFactor * sourceColor + (1.0 - IN.fogFactor) * fogColor;
 
-	return retColor;
+	return sourceColor;
 }
